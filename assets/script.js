85d39a35f1141105128d92af9c952e9afd8f1655
AndreBlankholm <ablankholm@gmail.com>
2022-03-23 16:58:02 -0500

    figured out how to start using function generatePassword

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,30 @@
 // Assignment code here
+ function generatePassword() {
+  var password = "";
+  var passwordLength = parseInt(prompt("Choose a length of at least 8 characters and no more than 128 characters."));
+  
+  while(!passwordLength || passwordLength < 8 || passwordLength > 128) {
+    passwordLength = parseInt(prompt("Choose a length of at least 8 characters and no more than 128 characters."));
+    console.log(passwordLength);//output of wrong answer
+  }
+  console.log(passwordLength);
 
+  var includeLowerCase = confirm("Do you want to include lowercase charaters in your password?");
+  var includeUpperCase = confirm("Do you want to include uppercase charaters in your password");
+  var includeNumeric = confirm("Do you want to include numeric charaters in your password");
+  var includeSpecial = confirm("Do you want to include special charaters in your password");
+  console.log(includeLowerCase);
+  console.log(includeUpperCase);
+  console.log(includeNumeric);
+  console.log(includeSpecial);
+  
+  if(){
+
+  }
+
+
+  return password;
+ }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
